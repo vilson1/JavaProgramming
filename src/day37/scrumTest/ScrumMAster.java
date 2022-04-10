8baa00294cdfd0944f72c6d3b8786143e14bd171
User <vilson.shehu@outlook.com>
2022-04-10 23:27:26 +0200

    Scrum MAster and Developer

---
+++ src/day37/scrumTest/ScrumMAster.java
@@ -0,0 +1,9 @@
+package day37.scrumTest;
+
+public class ScrumMAster extends Employee{
+    public ScrumMAster(String name, int age, char gender, int id, double salary, String companyName) {
+        super(name, age, gender, "Scrum Master", id, salary, companyName);
+    }
+
+
+}
