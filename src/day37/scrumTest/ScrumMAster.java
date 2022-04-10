9e4889897b32587f68244c55886b47a4da6dacff
User <vilson.shehu@outlook.com>
2022-04-10 23:51:27 +0200

    ScrumTeamClass

--- src/day37/scrumTest/ScrumMAster.java
+++
@@ -1,9 +0,0 @@
-package day37.scrumTest;
-
-public class ScrumMAster extends Employee{
-    public ScrumMAster(String name, int age, char gender, int id, double salary, String companyName) {
-        super(name, age, gender, "Scrum Master", id, salary, companyName);
-    }
-
-
-}
---
+++ src/day37/scrumTest/ScrumMaster.java
@@ -0,0 +1,9 @@
+package day37.scrumTest;
+
+public class ScrumMaster extends Employee{
+    public ScrumMaster(String name, int age, char gender, int id, double salary, String companyName) {
+        super(name, age, gender, "Scrum Master", id, salary, companyName);
+    }
+
+
+}
