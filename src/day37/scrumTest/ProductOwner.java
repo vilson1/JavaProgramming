4a2f6b0195e191b8adab1b593cb59dedea396f96
User <vilson.shehu@outlook.com>
2022-04-10 23:31:23 +0200

    BusinessAnalyst and ProductOwner

---
+++ src/day37/scrumTest/ProductOwner.java
@@ -0,0 +1,7 @@
+package day37.scrumTest;
+
+public class ProductOwner extends Employee{
+    public ProductOwner(String name, int age, char gender, int id, double salary, String companyName) {
+        super(name, age, gender, "Product Owner", id, salary, companyName);
+    }
+}
