79aa4bd0a71cedac48d55ec5e4778189bdc8d15d
Vilson <vilson.shehu@outlook.com>
2022-04-13 10:21:09 +0200

    Park lots

---
+++ src/day38/carTask/ParkingLot.java
@@ -0,0 +1,9 @@
+package day38.carTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Toyota toyota1=new Toyota("corrola",2015,7000,"Green",220000);
+        Tesla tesla1=new Tesla("X",2021,30000,"White",50000);
+        BMW bmw1=new BMW("X5",2022,50000,"Black",5000);
+    }
+}
